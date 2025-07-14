66f55396bca3df496bc5c8041cf212060da6fde0
etilester <129666338+etilester@users.noreply.github.com>
2025-07-14 14:30:49 +0200

    Initial commit

---
+++ 02_script/install-packages.R
@@ -0,0 +1,9 @@
+packages <- c("tidyverse", "plotly", "here", "knitr", "kableExtra", "readr", "lubridate", "conflicted")
+
+install_if_missing <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, repos = "https://cloud.r-project.org/")
+  }
+}
+
+lapply(packages, install_if_missing)
